6b221a462bdc7b99a990be17c0b7e4331f2d840d
amokfa <iaansagar@gmail.com>
2025-01-10 16:59:21 +0530

    wait for network to come up during initialization

--- lib/messenger.dart
+++ lib/messenger.dart
@@ -2,10 +2,10 @@
 import 'dart:collection';
 
 import 'package:bytebeam_flutter_sdk/lib.dart';
+import 'package:bytebeam_flutter_sdk/utils.dart';
 import 'package:mqtt_client/mqtt_client.dart';
 
 extension Messenger on BytebeamClient {
-
   /// Queue a message to be uploaded to cloud
   void sendMessage(BytebeamPayload payload) {
     if (messagesQueue.length >= maxQueuedMessages) {
@@ -16,28 +16,27 @@ extension Messenger on BytebeamClient {
 
   /// Internal
   Future<void> messengerTask() async {
-    try {
-      while (isConnected()) {
-        var payload = messagesQueue.firstOrNull;
-        if (payload == null) {
-          await MqttUtilities.asyncSleep(1);
+    while (isConnected()) {
+      var payload = messagesQueue.firstOrNull;
+      if (payload != null) {
+        var mqttMessage = MqttClientPayloadBuilder();
+        var message = payload.asMqttPayload();
+        String topic;
+        if (payload.stream == "action_status") {
+          topic = "/tenants/${credentials.project}/devices/${credentials.deviceId}/action/status";
         } else {
-          var mqttMessage = MqttClientPayloadBuilder();
-          var message = payload.asMqttPayload();
-          String topic;
-          if (payload.stream == "action_status") {
-            topic = "/tenants/${credentials.project}/devices/${credentials.deviceId}/action/status";
-          } else {
-            topic = "/tenants/${credentials.project}/devices/${credentials.deviceId}/events/${payload.stream}/jsonarray";
-          }
-          mqttMessage.addString(message);
+          topic = "/tenants/${credentials.project}/devices/${credentials.deviceId}/events/${payload.stream}/jsonarray";
+        }
+        mqttMessage.addString(message);
+        try {
           mqttClient.publishMessage(topic, MqttQos.atLeastOnce, mqttMessage.payload!);
           messagesQueue.removeFirst();
+        } catch (e) {
+          print("BYTEBEAM::WARN mqtt network error: $e");
+          await MqttUtilities.asyncSleep(1);
         }
-      }
-    } catch (e) {
-      if (isConnected()) {
-        print("BYTEBEAM::ERROR messenger task ended with error: $e");
+      } else {
+        await MqttUtilities.asyncSleep(1);
       }
     }
   }
@@ -45,16 +44,10 @@ extension Messenger on BytebeamClient {
   /// Internal
   Future<void> deviceShadowTask() async {
     int sequence = 1;
-    try {
-      while (isConnected()) {
-        sendMessage(BytebeamPayload("device_shadow", sequence, {}));
-        sequence += 1;
-        await MqttUtilities.asyncSleep(15);
-      }
-    } catch (e) {
-      if (isConnected()) {
-        print("BYTEBEAM::ERROR device shadow task ended with error: $e");
-      }
+    while (isConnected()) {
+      sendMessage(BytebeamPayload("device_shadow", sequence, {}));
+      sequence += 1;
+      await MqttUtilities.asyncSleep(15);
     }
   }
 
